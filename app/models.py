66b2caa9a7d3cbbfcc296a47eae0f4f40870d698
Brandon Apol <brandonapol@cedarville.edu>
2022-03-12 22:21:26 -0900

    Additional auth functionality; register forms

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 from flask_login import UserMixin
+from app import login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,7 +12,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash
+        self.password_hash = generate_password_hash(password)
         print(self.password_hash)
     
     def check_password(self, password):
